2d130733ea9cb687d33ff50260efb83029bb2da2
unknown <andersonlantunes@gmail.com>
2020-07-05 19:22:50 -0300

    Adicionado arquivo para alerts

---
+++ js/alerts.js
@@ -0,0 +1,27 @@
+const AlertSigningUp = () => {
+  let timerInterval;
+  Swal.fire({
+    title: "Cadastrando...",
+    timerProgressBar: true,
+    onBeforeOpen: () => {
+      Swal.showLoading();
+      timerInterval = setInterval(() => {
+        const content = Swal.getContent();
+        if (content) {
+          const b = content.querySelector("b");
+          if (b) {
+            b.textContent = Swal.getTimerLeft();
+          }
+        }
+      }, 100);
+    },
+    onClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+    }
+  });
+};
