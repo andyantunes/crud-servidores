f91a8e839e7428d67a8dee744a60c7af7470fe1e
unknown <andersonlantunes@gmail.com>
2020-07-05 19:23:30 -0300

    Criado arquivo para cadastro de clientes

---
+++ php/cadastroUsuario.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+session_start();
+
+ob_start();
+include_once("databaseConnection.php");
+
+$name = $_POST['name'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+$sql = 'insert into users (UserName, UserPassword, UserEmail, UserPhone) 
+        values("'.$name.'", "'.$password.'", "'.$email.'", "'.$phone.'")';
+$rs = mysqli_query($mysqli, $sql);
+
+if($rs) {
+    $return = true;
+} else {
+    $return =  "Error updating record: " . mysqli_error($mysqli);
+}
+
+mysqli_close($mysqli);
+
+header('Content-Type: application/json');
+echo json_encode(array(
+    'return' => $rs
+));
